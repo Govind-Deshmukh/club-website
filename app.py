b43b2f075de8e9cbf612034115fcb7beb3b0cde4
Govind-Deshmukh <govinddeshmukh2001@gmail.com>
2022-10-08 23:04:59 +0530

    ok

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 from flask import Flask, render_template, request, redirect,session,url_for, flash,make_response,jsonify
 from flask_session import Session
-import jwt
-from datetime import datetime, timedelta
 from flask_cors import CORS
-import mysql.connector
 import sqlite3
+from flask_admin import Admin
+# import jwt
+# from datetime import datetime, timedelta
+# import mysql.connector
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import UserMixin
 
 
 app = Flask(__name__)
+# database configuration 
 app.secret_key = 'iamfuckingcreazy'
+# session configuration 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+# cors config    
 CORS(app)
 
 
 
 conn = sqlite3.connect('database.db',check_same_thread=False)
-
+c = conn.cursor()
 
 
 @app.route('/', methods=['POST','GET'])
@@ -69,72 +75,65 @@ def GitSearch():
         return redirect(url, 301)
 
 
-@app.route('/login',methods=['GET','POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('entry/login.html')
-        # session["name"] = request.form("name")
-    if request.method == 'POST':
-        flash(f"Record Saved!", "success")
-        return 'login'
+# @app.route('/login',methods=['GET','POST'])
+# def login():
+#     if request.method == 'GET':
+#         return render_template('entry/login.html')
+#         # session["name"] = request.form("name")
+#     if request.method == 'POST':
+#         flash(f"Record Saved!", "success")
+#         return 'login'
 
 @app.route('/register',methods=['GET','POST'])
 def register():
     if request.method == 'GET':
         return render_template('entry/register.html')
-
     if request.method == 'POST':
         if request.form['password'] != request.form['confpassword']:
-            print(request.form['password']," ",request.form['confpassword'])
-            flash("Password and Confirm Password does not match") 
+            flash('Password and Confirm Password does not match', 'error')
             return redirect(url_for('register'))
-        # else:
-        #     email = request.form['email']
-        #     verify_query = "select * from club_users where email = "+str(email)+";"
-        #     print(verify_query)
-        #     conn.execute(verify_query)
-        #     conn.commit
-        #     user = conn.fetchall()
-        #     print("email verified")
-        #     if user:
-        #         flash('User already exists')
-        #         return redirect(url_for('register'))
         else:
-            name = request.form['name' ]
             email = request.form['email']
-            mobile = request.form['mobile']
-            domain = request.form['domain']
-            year = request.form['year']
-            password = request.form['password']
-            print(name,email,mobile,domain,year,password)
-            print(type(email))
-            conn.execute(f'''INSERT INTO club_users (name,email,mobile,doamin,year,password) 
-            VALUES ({name},{email},{mobile},{domain},{year},{password});''')
-            conn.commit
-            flash('You are now registered and can log in')
-            print("data inserted")
-            return redirect(url_for('register'))
+            verify_query = "select * from club_users where email = '"+str(email)+"';"
+            user = conn.execute(verify_query)
+            if user:
+                flash('User already exists','error')
+                return redirect(url_for('register'))
+            else:
+                name = request.form['name' ]
+                email = request.form['email']
+                mobile = request.form['mobile']
+                domain = request.form['domain']
+                year = request.form['year']
+                password = request.form['password']
+                c.execute(f"INSERT INTO club_users (name,email,mobile,domain,year,password) VALUES ('{name}','{email}','{mobile}','{domain}','{year}','{password}');")
+                conn.commit()
+                flash('You are now registered and can log in','success')
+                return redirect(url_for('login'))
 
 
-# @app.route('/login' , methods=['POST'])
-# def login():
-#     if request.method == 'POST':
-#         dump = request.get_json()
-#         username = dump['username']
-#         password =  dump['password']
-#         conn.execute("SELECT * FROM users WHERE username = '"+username+"' AND password = '"+password+"';")
-#         data = conn.fetchall()
-#         if (len(data)==0 or len(data)>1):
-#             resp = jsonify({'message' : 'Invalid username or password'})
-#             resp.status_code = 400
-#             return resp
-#         else:
-#             # added username in session
-#             session['username'] = username
-#             # return jwt token 
-#             token = jwt.encode({'public_id': username,'exp' : datetime.utcnow() + timedelta(hours= 12)}, app.config['SECRET_KEY'])
-#             return make_response(jsonify({'token' : token.decode('UTF-8'),'username' : username }), 201)
+@app.route('/login' , methods=['POST','GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('entry/login.html')
+        # session["name"] = request.form("name")
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        data = conn.execute("SELECT * FROM club_users WHERE email = '"+email+"' AND password = '"+password+"';")
+        if (len(data)==0 or len(data)>1):
+            flash('Invalid username or password','error')
+            return redirect(url_for('login'))
+        else:
+            # added username in session
+            session["user"] = email
+            flash('Login successful','success')
+            return redirect(url_for())
 
+# logout route
+@app.route('/dashboard', methods=['GET','POST'])
+def dashboard():
+    return render_template('Dashboard/dashboard.html')
 
 # logout route
 @app.route('/logout', methods=['GET','POST'])
